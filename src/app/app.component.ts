9df445bb059b513a1e051848b9dc6077b9baeca3
Sachin Yerrawar (syerrawa) <syerrawa@cisco.com>
2022-01-05 21:19:31 -0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-http-app';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    this.http.get('./assets/data.json').subscribe((res: any) => {
+      this.title = `${res.title}`;
+    });
+  }
 }
